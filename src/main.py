4fcc19c9225a74e03861ae72a98705d409ab57f5
SohagCity <sohag.miah@city.ac.uk>
2021-12-17 22:38:30 +0000

    fix delcaration

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from path_finder import PathFinder
 
 
-path_finder = PathFinder(4, 4, 0)
+path_finder = PathFinder(4, 4, 0, a = 0, b = 10)
 path_finder.print_board()
 
 print(path_finder.dijkstra_path())
